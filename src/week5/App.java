fba095c94f67c0634bc40bf3c0746205915d4a70
countkhalil <152437499+countkhalil@users.noreply.github.com>
2023-12-16 11:36:49 -0700

    Added Scanner
    Add Scanner to App Main

--- src/week5/App.java
+++ src/week5/App.java
@@ -1,5 +1,8 @@
 package week5;
 
+import java.util.Scanner;
+
+//Code by Khalil McVay
 public class App {
 
 	public static void main(String[] args) {
@@ -7,13 +10,34 @@ public class App {
 		Logger asterisk = new AsteriskLogger();
 		Logger spaced = new SpacedLogger();
 		
+		
+		
+		
+		
+		System.out.println("Press Enter to Continue");
+		Scanner sc= new Scanner(System.in);
+		String nextLine = sc.nextLine();
 	
 		asterisk.log("Don't Read This Message");
-		System.out.println(" ");
+		System.out.println("  ");
+		
+		 sc= new Scanner(System.in);
+		nextLine= sc.nextLine();
+		
+		
 		asterisk.error("You Have Read The Message. Please Look Away");
 		System.out.println(" ");
+		
+		 sc= new Scanner(System.in);
+			nextLine= sc.nextLine();
+			
+			
 		spaced.log("You Did Not Look Away");
 		System.out.println(" ");
+	
+		sc= new Scanner(System.in);
+			nextLine= sc.nextLine();
+			
 		spaced.error("LOOK BEHIND YOU");
 		System.out.println(" ");
 		
